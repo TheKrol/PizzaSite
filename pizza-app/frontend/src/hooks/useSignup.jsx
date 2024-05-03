588d179c22e965b088577ad8cb8bb6c8707f0108
TheKrol <TheKrol@users.noreply.github.com>
2024-05-03 09:58:37 -0400

    Get login working on site

--- pizza-app/frontend/src/hooks/useSignup.jsx
+++ pizza-app/frontend/src/hooks/useSignup.jsx
@@ -10,7 +10,7 @@ export const useSignup = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch(" https://thekrol.github.io/PizzaSite/user/signup", {
+    const response = await fetch("https://thekrol.github.io/PizzaSite/user/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password, role })
